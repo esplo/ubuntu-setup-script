ed82ead8594df00c19dc03420b87965022a4c200
esplo <you@example.com>
2016-07-30 13:36:14 +0900

    initial commit

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -eux
+
+# software install
+sudo apt-get -y update
+INSTALL_SOFTS=(vim git steam openjdk-8-jdk icedtea-netx)
+for soft in ${INSTALL_SOFTS[@]}
+do
+  sudo apt-get -y install ${soft}
+done
+
+# google japanese input
+sudo apt-get -y install $(check-language-support)
+
+# install chrome
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+# install CLion
+wget https://download.jetbrains.com/cpp/CLion-2016.2.tar.gz
+tar zxvf CLion*.tar.gz
+mkdir -p ~/ide
+mv clion-*/* ~/ide/clion/
+rm -r clion-*
+
+# install dropbox
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -O dropbox.deb
+sudo dpkg -i dropbox.deb
+sudo apt-get -f -y install
+sudo dpkg -i dropbox.deb
+rm dropbox.deb
+
+# settings
+dconf reset /org/gnome/settings-daemon/plugins/keyboard/active
+dconf write /org/gnome/desktop/input-sources/xkb-options "['ctrl:nocaps']"
+
+# for steam
+echo "LD_PRELOAD='/usr/$LIB/libstdc++.so.6' DISPLAY=:0 steam" > run_steam.sh
+chmod 755 run_steam.sh
+
+# folder name
+LANG=C xdg-user-dirs-gtk-update
+
+# thunderbird profile
+mkdir -p ~/.thunderbird
+cp -r ./backup_thunderbird/* ~/.thunderbird/
+
+
+# mozc
+#  Henkan/Muhenkan 
+
+# cleanup
+sudo apt-get -y autoremove
