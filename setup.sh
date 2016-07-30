0253620d224b4c61e9c81ee773d5b93848f96be2
esplo <you@example.com>
2016-07-30 14:06:00 +0900

    add hybernate menu

--- setup.sh
+++ setup.sh
@@ -51,5 +51,20 @@ cp -r ./backup_thunderbird/* ~/.thunderbird/
 # mozc
 #  Henkan/Muhenkan 
 
+# hybernate menu
+cat << EOT | sudo tee -a /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
+[Re-enable hibernate by default in upower]
+Identity=unix-user:*
+Action=org.freedesktop.upower.hibernate
+ResultActive=yes
+
+[Re-enable hibernate by default in logind]
+Identity=unix-user:*
+Action=org.freedesktop.login1.hibernate;org.freedesktop.login1.handle-hibernate-key;org.freedesktop.login1;org.freedesktop.login1.hibernate-multiple-sessions;org.freedesktop.login1.hibernate-ignore-inhibit
+ResultActive=yes
+EOT
+
 # cleanup
 sudo apt-get -y autoremove
+
+
